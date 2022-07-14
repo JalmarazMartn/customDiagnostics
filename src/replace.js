0bdc8aef99c7dbe473c5693d45c8f2f30c4fc80d
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2022-07-14 19:02:00 +0200

    Dialog and optimization

--- src/replace.js
+++ src/replace.js
@@ -6,43 +6,51 @@ module.exports = {
     }
 }
 
-async function replaceRulesInAllDocuments(rules,fileExtension) {
+async function replaceRulesInAllDocuments(rules, fileExtension) {
     //const getRules = require('./getRules.js');
     //let rules = getRules.getRules();
     if (!rules) {
         return;
     }
-    for (let i = 0; i < rules.length; i++) {
-        let customRule = rules[i];
-        const documents = await vscode.workspace.findFiles('**/*.'+fileExtension);
-        for (let j = 0; j < documents.length; j++) {
-            try
-            {
-                let document = await vscode.workspace.openTextDocument(documents[j]);
+
+    //show progress window
+    vscode.window.withProgress({
+        cancellable: true,
+        location: vscode.ProgressLocation.Notification,
+        title: 'Replacing rules in documents',
+      }, async (progress) => {        
+    const documents = await vscode.workspace.findFiles('**/*.' + fileExtension);
+    for (let j = 0; j < documents.length; j++) {        
+        try {
+            let document = await vscode.workspace.openTextDocument(documents[j]);
+            progress.report({ message: 'Replacing rules in document ' + document.fileName });
+            for (let i = 0; i < rules.length; i++) {
+                let customRule = rules[i];
+
                 await replaceRuleInDocument(customRule, document);
             }
-            catch (error) {
-                if (error.message.search(/binary/i) < 0) {
-                    console.log(error);
-                }
+        }
+        catch (error) {
+            if (error.message.search(/binary/i) < 0) {
+                console.log(error);
             }
         }
-    }
+    }            
+})
+
+    vscode.window.showInformationMessage("Replacing process ended.");
 }
-async function replaceRuleInDocument(customRule, document) {
-    if (!customRule) {
+async function replaceRuleInDocument(replaceRule, document) {
+    if (!replaceRule) {
         return;
     }
-    if (customRule.language)
-    {
-        if (document.languageId !== customRule.language) {
-            return;
-        }
+    if (document.getText().search(replaceRule.searchExpresion) < 0) {
+        return;
     }
     for (let i = 0; i < document.lineCount; i++) {
         let lineText = document.lineAt(i).text;
-        const regex = new RegExp(customRule.searchExpresion, 'i');
-        let replaceText = lineText.replace(regex, customRule.replaceExpression);
+        const regex = new RegExp(replaceRule.searchExpresion, 'gi');
+        let replaceText = lineText.replace(regex, replaceRule.replaceExpression);
         if (replaceText != lineText) {
             let edit = new vscode.WorkspaceEdit();
             edit.replace(document.uri, new vscode.Range(i, 0, i, lineText.length), replaceText);
@@ -62,11 +70,11 @@ function pickAndExcuteRuleset() {
         ruleSetNames.push(ruleSets[i].name);
     }
     //show vscode code picker with the ruleSetNames        
-	vscode.window.showQuickPick(ruleSetNames).then(async (value) => {
-		if (value) {
+    vscode.window.showQuickPick(ruleSetNames).then(async (value) => {
+        if (value) {
             let rules = getRules.getRulesFromRuleSetName(value);
             const fileExtension = getRules.getFileExtensionFormRuleSetName(value);
-            await replaceRulesInAllDocuments(rules,fileExtension);
+            await replaceRulesInAllDocuments(rules, fileExtension);
         }
     }
     );
