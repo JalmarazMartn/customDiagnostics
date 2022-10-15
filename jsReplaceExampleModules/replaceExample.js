705361539fb46b21ae341e875d7da24b9683f6ed
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2022-10-15 06:54:29 +0200

    new function example. Snippet fixing

--- jsReplaceExampleModules/replaceExample.js
+++ jsReplaceExampleModules/replaceExample.js
@@ -4,6 +4,9 @@ module.exports = {
     },
     CreateReservEntryFor: function (exprMatch) {
         return CreateReservEntryFor(exprMatch);
+    },
+    addLayoutFolderIfNotExists: function (exprMatch) {
+        return addLayoutFolderIfNotExists(exprMatch);
     }
 }
 function CreateReservEntryFor(exprMatch) {
@@ -16,8 +19,23 @@ function CreateReservEntryFor(exprMatch) {
         result[5] + result[6] + result[7] + result[8] + result[9];
     strTo = strTo + result[0] + result[1] + result[2] + result[3] + result[4] + result[5] +
         result[6] + result[7] + ',ReservationEntry);';
-    const trackingSetting = 'ReservationEntry."Serial No." := ' + result[8].replace(CommaAndOthers,'') + ';\r\n' +
-        'ReservationEntry."Lot No." := ' + result[9].replace(CommaAndOthers,'') + ';\r\n';
-    return  trackingSetting + exprMatch.replace(strFrom, strTo);
+    const trackingSetting = 'ReservationEntry."Serial No." := ' + result[8].replace(CommaAndOthers, '') + ';\r\n' +
+        'ReservationEntry."Lot No." := ' + result[9].replace(CommaAndOthers, '') + ';\r\n';
+    return trackingSetting + exprMatch.replace(strFrom, strTo);
+
+}
+function addLayoutFolderIfNotExists(exprMatch) {
+    const aditionalString = 'src/report/layout/';
+    if (stringAlreadyExists(exprMatch, aditionalString)) {
+        return exprMatch;
+    }
+    return exprMatch.replace(new RegExp("Layout\\s*=\\s*'.\/(.*)\'"), "Layout = '.\/src\/report\/layout\/$1'");
+}
+function stringAlreadyExists(exprMatch = '', newString = '') {
+    const newStringRegExp = new RegExp(newString, 'gmi');
+    if (exprMatch.search(newStringRegExp) >= 0) {
+        return true;
+    }
+    return false;
 
 }
